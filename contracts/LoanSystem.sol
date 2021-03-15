01300b41e8de4d6cb97c79a7ca9f7cb83c54a941
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-03-15 20:26:31 +0100

    updates

---
+++ contracts/LoanSystem.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.4;
+
+contract LoanSystem {
+    address public owner;
+
+  mapping (bytes32=>address[]) public ownerMap;
+  mapping (address=>bytes32[]) public loanMap;
+
+  constructor() public {
+      owner = msg.sender;
+  }
+  
+  function addData(bytes32 memory document) public {
+    address[] storage owners = ownerMap[document];
+    for( uint i = 0; i < owners.length; i++){
+      if(owners[i] == msg.sender)
+        return;
+    }
+    ownerMap[document].push(msg.sender);
+    uint count = loanMap[msg.sender].length;
+    for( uint i = 0; i < count; i++){
+      if (loanMap[msg.sender][i] == document)
+        return;
+    }
+    loanMap[msg.sender].push(document);
+  }
+
+  function getLoanCount(address person) public view returns(uint) {
+    return loanMap[person].length;
+  }
+
+  function getOwnerCount(bytes32 memory hash) public view returns(uint) {
+    return ownerMap[hash].length;
+  }
+  
+  function getOwnerByPosition(bytes32 memory hash, uint index) public view returns(address) {
+    return ownerMap[hash][index];
+  }
+  
+}
